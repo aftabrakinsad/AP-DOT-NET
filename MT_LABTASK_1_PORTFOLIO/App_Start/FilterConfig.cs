b40a3ada9b1c1435334665a2f268c45485fca332
Rakin Sad Aftab <20-41991-1@student.aiub.edu>
2022-09-28 00:22:35 +0600

    MID TERM LAB TASK

---
+++ MT_LABTASK_1_PORTFOLIO/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace MT_LABTASK_1_PORTFOLIO
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ MT_LABTASK_1_PORTFOLIO/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MT_LABTASK_1_PORTFOLIO.MvcApplication" Language="C#" %>
