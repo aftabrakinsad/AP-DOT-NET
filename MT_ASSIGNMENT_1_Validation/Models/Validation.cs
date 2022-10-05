d571c28ffeb078509480f3af30fba8f8fbdb1587
aftabrakinsad <20-41991-1@student.aiub,edu>
2022-10-05 16:43:43 +0600

    Assignment 1 Form Validation

--- MT_ASSIGNMENT_1_Validation/Models/Validation.cs
+++ MT_ASSIGNMENT_1_Validation/Models/Validation.cs
@@ -8,27 +8,34 @@ namespace MT_ASSIGNMENT_1_Validation.Models
 {
     public class Validation
     {
+        [Display(Name = "Full Name")]
         [Required(ErrorMessage = "Don't Have any name?")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Name should contain 3 to 50 characters")]
         public string fullname { get; set; }
 
+        [Display(Name = "AIUB ID")]
         [Required(ErrorMessage = "No ID, No Entry!")]
         public string aiubid { get; set; }
 
+        [Display(Name = "Gender")]
         [Required(ErrorMessage = "He or She?")]
         public string gender { get; set; }
 
+        [Display(Name = "AIUB Mail")]
         [Required(ErrorMessage ="xx-xxxxx-x@student.aiub.edu mail is required.")]
         public string aiubmail { get; set; }
 
+        [Display(Name = "Password")]
         [Required(ErrorMessage ="We can't let you in without password.")]
         [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = "Please maintain 1 uppercase, 1 lowercase, 1 special charater and 1 number with minimum 8 digits")]
         public string pass { get; set; }
 
+        [Display(Name = "Confirm Password")]
         [Required(ErrorMessage = "Until the password matched.")]
         [Compare("pass", ErrorMessage ="Are you lost? Password didn't match.")]
         public string cpass { get; set; }
 
+        [Display(Name = "Age")]
         [Required(ErrorMessage = "18+?")]
         public string dob { get; set; }
     }
