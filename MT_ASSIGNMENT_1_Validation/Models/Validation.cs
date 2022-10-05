6ac4ed537eea8c6540bdc6e9c0c34de416d22033
aftabrakinsad <20-41991-1@student.aiub,edu>
2022-10-05 14:42:03 +0600

    Assignement 1 Form Validation

--- MT_ASSIGNMENT_1_Validation/Models/Validation.cs
+++ MT_ASSIGNMENT_1_Validation/Models/Validation.cs
@@ -10,20 +10,27 @@ namespace MT_ASSIGNMENT_1_Validation.Models
     {
         [Required(ErrorMessage = "Don't Have any name?")]
         [StringLength(50, MinimumLength = 3, ErrorMessage = "Name should contain 3 to 50 characters")]
+        
         public string fullname { get; set; }
         [Required(ErrorMessage = "No ID, No Entry!")]
+        
         public string aiubid { get; set; }
         [Required(ErrorMessage = "He or She?")]
+        
         public string gender { get; set; }
         [Required(ErrorMessage =".edu mail is required.")]
+        
         public string aiubmail { get; set; }
         [Required(ErrorMessage ="We can't let you in without password.")]
         [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = "Please maintain 1 uppercase, 1 lowercase, 1 special charater and 1 number with minimum 8 digits")]
+        
         public string pass { get; set; }
         [Required(ErrorMessage = "Until the password matched.")]
         [Compare("pass")]
+        
         public string cpass { get; set; }
         [Required(ErrorMessage = "18+?")]
+        
         public string dob { get; set; }
     }
 }
