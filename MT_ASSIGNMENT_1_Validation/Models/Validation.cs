4facc8a800e8a95fb209d73460885588208ed003
aftabrakinsad <20-41991-1@student.aiub,edu>
2022-10-08 22:32:43 +0600

    Assignment 1 Form Validation

--- MT_ASSIGNMENT_1_Validation/Models/Validation.cs
+++ MT_ASSIGNMENT_1_Validation/Models/Validation.cs
@@ -25,6 +25,8 @@ namespace MT_ASSIGNMENT_1_Validation.Models
 
         [Display(Name = "AIUB Mail")]
         [Required(ErrorMessage ="xx-xxxxx-x@student.aiub.edu mail is required.")]
+        [RegularExpression("^[1-9][0-9]-[0-9][0-9][0-9][0-9][0-9]-[1-3]@student.aiub.edu+", ErrorMessage ="Aiub er deya mail dao vaya")]
+        [Compare("aiubid + aiubmail")]
         public string aiubmail { get; set; }
 
         [Display(Name = "Password")]
