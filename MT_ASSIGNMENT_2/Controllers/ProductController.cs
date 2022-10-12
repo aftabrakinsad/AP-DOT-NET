adbd09e39a353e738c9f09ba424f31e1512a9a28
aftabrakinsad <20-41991-1@student.aiub,edu>
2022-10-12 23:01:52 +0600

    Assignment 2

---
+++ MT_ASSIGNMENT_2/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MT_ASSIGNMENT_2.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Product()
+        {
+            return View();
+        }
+    }
+}
---
+++ MT_ASSIGNMENT_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MT_ASSIGNMENT_2.MvcApplication" Language="C#" %>
