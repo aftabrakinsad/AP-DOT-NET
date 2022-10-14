d8418da42ed01103c3621c273694f5b8183af43a
aftabrakinsad <20-41991-1@student.aiub,edu>
2022-10-14 22:03:32 +0600

    Assignment 2

--- MT_ASSIGNMENT_2/Controllers/ProductController.cs
+++ MT_ASSIGNMENT_2/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MT_ASSIGNMENT_2.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,69 @@ namespace MT_ASSIGNMENT_2.Controllers
 {
     public class ProductController : Controller
     {
-        // GET: Product
+        public ActionResult Index()
+        {
+            var database = new MID_ASSIGNMENT_TWOEntities();
+            var Products = database.Products.ToList();
+            return View(Products);
+        }
+
+        [HttpGet]
         public ActionResult Products()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Products(Product ppt)
+        {
+            //add to db
+            var database = new MID_ASSIGNMENT_TWOEntities();
+            database.Products.Add(ppt);
+            database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+            var database = new MID_ASSIGNMENT_TWOEntities();
+            var ext = (from pt in database.Products
+                       where pt.ID == ID
+                       select pt).SingleOrDefault();
+            return View(ext);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product p)
+        {
+            var database = new MID_ASSIGNMENT_TWOEntities();
+            var ext = (from pt in database.Products
+                       where pt.ID == p.ID
+                       select pt).SingleOrDefault();
+            ext.Name = p.Name;
+            ext.Price = p.Price;
+            ext.Qty = p.Qty;
+            database.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int ID)
+        {
+            using (var database = new MID_ASSIGNMENT_TWOEntities())
+            {
+                var pd = database.Products.FirstOrDefault(x => x.ID == ID);
+                database.Products.Remove(pd);
+                database.SaveChanges();
+                return RedirectToAction("Index");
+            }
         }
     }
 }
