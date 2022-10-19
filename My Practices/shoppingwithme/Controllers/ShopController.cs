0c0726eac6483ac834cea1f72105ea67ccc1811e
aftabrakinsad <20-41991-1@student.aiub,edu>
2022-10-19 13:03:04 +0600

    My Practises

---
+++ My Practices/shoppingwithme/Controllers/ShopController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using shoppingwithme.Database;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace shoppingwithme.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Letsdoshop()
+        {
+            var database = new shoppingwithmeEntities();
+            var Product = database.products.ToList();
+            return View(Product);
+        }
+    }
+}
