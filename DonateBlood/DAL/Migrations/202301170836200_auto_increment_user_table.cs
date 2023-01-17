bd91405e218718af7e9228cd359cd9f29627aa34
Hell-Fire <aftabhpy@gmail.com>
2023-01-17 15:05:27 +0600

    CRUD operation on Blood Donation-CF

---
+++ DonateBlood/DAL/Migrations/202301170836200_auto_increment_user_table.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class auto_increment_user_table : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Users", "ID", c => c.Int(nullable: false, identity: true));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Users", "ID", c => c.Int(nullable: false));
+        }
+    }
+}
