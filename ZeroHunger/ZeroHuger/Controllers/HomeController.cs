0daa6e66516e9705771adf94c4267d66a88aba1a
aftabrakinsad <20-41991-1@student.aiub.edu>
2023-01-21 23:19:04 +0600

    You won't be hungry anymore

--- ZeroHunger/ZeroHuger/Controllers/HomeController.cs
+++ ZeroHunger/ZeroHuger/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,23 +11,66 @@ namespace ZeroHuger.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult EmployeeLogin()
+        public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Restaurant()
+        public ActionResult EmployeeLogin(Employee_loginDTO emplog)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            var emp = Emp_Log_Service.Get(emplog.emp_id);
+            if (emp != null)
+            {
+                if (emp.emp_pass.Equals(emplog.emp_pass))
+                {
+                    Session["logged"] = emp.emp_id;
+                    Session["logged"] = emp.emp_pass;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                TempData["msg"] = "Invalid ID or Password";
+            }
+            return RedirectToAction("EmployeeLogin");
         }
 
-        public ActionResult Company()
+        public ActionResult CompanyLogin(Company_loginDTO comlog)
         {
-            ViewBag.Message = "Your contact page.";
+            var com = Com_Log_Service.Get(comlog.com_id);
+            if (com != null)
+            {
+                if (com.com_pass.Equals(comlog.com_pass))
+                {
+                    Session["logged"] = com.com_id;
+                    Session["logged"] = com.com_pass;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                TempData["msg"] = "Invalid ID or Password";
+            }
+            return RedirectToAction("CompanyLogin");
+        }
 
-            return View();
+        public ActionResult RestaurantLogin(Restaurant_loginDTO reslog)
+        {
+            var res = Res_Log_Service.Get(reslog.res_id);
+            if (res != null)
+            {
+                if (res.res_pass.Equals(reslog.res_pass))
+                {
+                    Session["logged"] = res.res_id;
+                    Session["logged"] = res.res_pass;
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            else
+            {
+                TempData["msg"] = "Invalid ID or Password";
+            }
+            return RedirectToAction("RestaurantLogin");
         }
     }
 }
