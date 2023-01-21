8de7c8ab19911e4fae48d57c6b56a2fd19df1ed6
aftabrakinsad <20-41991-1@student.aiub.edu>
2023-01-21 17:11:20 +0600

    You won't be hungry anymore

--- ZeroHunger/DAL/Migrations/Configuration.cs
+++ ZeroHunger/DAL/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 ﻿namespace DAL.Migrations
 {
+    using DAL.ZH_EF_CF.Models;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -14,10 +16,29 @@
 
         protected override void Seed(DAL.ZH_EF_CF.ZeroHungerEntities context)
         {
-            //  This method will be called after migrating to the latest version.
+            //Employee login info
+            List<Employee_login> employee_Logins = new List<Employee_login>();
+            for (int i = 1; i <= 1; i++)
+            {
+                employee_Logins.Add(new Employee_login()
+                {
+                    emp_id = 7956,
+                    emp_pass = "88888"
+                });
+            }
+            context.Employee_Logins.AddOrUpdate(employee_Logins.ToArray());
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
-            //  to avoid creating duplicate seed data.
+            //Restaurant login info
+            List<Restaurant_login> restaurant_Logins = new List<Restaurant_login>();
+            for (int i = 1; i <= 1; i++)
+            {
+                restaurant_Logins.Add(new Restaurant_login()
+                {
+                    res_id = 22222,
+                    res_pass = "Restaurant"
+                });
+            }
+            context.Restaurant_Logins.AddOrUpdate(restaurant_Logins.ToArray());
         }
     }
 }
