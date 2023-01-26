ea56a973bbfbf95785ba8016e6aa4bf26f121655
aftabrakinsad <20-41991-1@student.aiub.edu>
2023-01-26 19:41:17 +0600

    Care your health

---
+++ HCSystem/HealthCareSystem/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HealthCareSystem.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(CatrgoryDTO cat)
+        {
+            CategoryService.CreateCategory(cat);
+            return RedirectToAction("Dashboard");
+        }
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
