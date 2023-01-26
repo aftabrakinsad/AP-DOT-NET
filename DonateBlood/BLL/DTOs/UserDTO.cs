a262b2085a6e78c1b9acf4e62ca73d1c9662892a
aftabrakinsad <20-41991-1@student.aiub.edu>
2023-01-26 18:32:20 +0600

    CRUD - Blood Donation

---
+++ DonateBlood/BLL/DTOs/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    public class UserDTO
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
